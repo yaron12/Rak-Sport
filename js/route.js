66bb648338ce8d2d7890d109a46d492da9b95681
yaronbensimon1@gmail.com <026535898>
2017-01-07 22:54:13 +0200

    first

---
+++ js/route.js
@@ -0,0 +1,27 @@
+(function() {
+	angular.module("app").config(route);
+	function route($routeProvider) {
+		$routeProvider
+		.when('/', {
+			templateUrl : 'App/view/leageStatus.html'
+
+		}).when('/teams', {
+			templateUrl : 'App/view/teams.html'
+			//controller : "contactUs"
+			//template:"<h1>this is about page</h1>"
+
+		}).when('/statistics', {
+			templateUrl : 'App/view/statistics.html'
+
+		}).when('/games', {
+			templateUrl : 'App/view/games.html'
+			
+		}).otherwise({
+			redirectTo : '/'
+		});
+}
+	
+	
+
+})();
+
